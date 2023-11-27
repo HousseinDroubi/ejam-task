cbf55fe1ab21c440d49a48e4e390175671ed3fb8
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-11-27 20:28:16 +0200

    feat(script): implement managing counter

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,26 @@ const header_paragraph_4 = document.getElementById("header_paragraph_4");
 const arrow_left = document.getElementById("arrow_left");
 const arrow_right = document.getElementById("arrow_right");
 
+let counter = 1;
+
 const arrowLeft = () => {
+  if (counter == 1) {
+    counter = 4;
+  } else {
+    counter--;
+  }
   console.log("arrowLeft");
+  console.log(counter);
 };
 
 const arrowRight = () => {
+  if (counter == 4) {
+    counter = 1;
+  } else {
+    counter++;
+  }
   console.log("arrowRight");
+  console.log(counter);
 };
 arrow_left.addEventListener("click", () => arrowLeft());
 arrow_right.addEventListener("click", () => arrowRight());
