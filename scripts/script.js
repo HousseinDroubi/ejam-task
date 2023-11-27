4c0e2900961412da998ffda893e1b7f8cda17759
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-11-27 21:05:48 +0200

    feat(script): implement a default function to get screen size and show first paragraph if there's need

--- scripts/script.js
+++ scripts/script.js
@@ -39,5 +39,27 @@ const arrowRight = () => {
   showParagraph(counter);
 };
 
+const handleScreenWidth = () => {
+  const screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  if (screenWidth <= 360) {
+    console.log("Hello");
+    showParagraph(1);
+  } else {
+    for (let i = 1; i <= 4; i++) {
+      document
+        .getElementById(`header_paragraph_${i}`)
+        .classList.remove("display-none");
+      document.getElementById(`header_paragraph_${i}`).classList.add("flex");
+    }
+  }
+};
+
+handleScreenWidth();
+
+window.addEventListener("resize", handleScreenWidth);
 arrow_left.addEventListener("click", () => arrowLeft());
 arrow_right.addEventListener("click", () => arrowRight());
