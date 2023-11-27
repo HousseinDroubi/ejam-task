952b57e797cc9e96e73b6e49b8ee23f259f6e8c9
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-11-27 20:21:18 +0200

    feat(script): get header paragraphs by their ids

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const header_paragraph_1 = document.getElementById("header_paragraph_1");
+const header_paragraph_2 = document.getElementById("header_paragraph_2");
+const header_paragraph_3 = document.getElementById("header_paragraph_3");
+const header_paragraph_4 = document.getElementById("header_paragraph_4");
