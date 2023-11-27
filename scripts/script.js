da84ae17728ae5613108d8816fe9c7ed7b7ce7c5
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-11-27 20:25:45 +0200

    feat(script): implement basic function for icons 'onClick' listeners

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ const header_paragraph_3 = document.getElementById("header_paragraph_3");
 const header_paragraph_4 = document.getElementById("header_paragraph_4");
 const arrow_left = document.getElementById("arrow_left");
 const arrow_right = document.getElementById("arrow_right");
+
+const arrowLeft = () => {
+  console.log("arrowLeft");
+};
+
+const arrowRight = () => {
+  console.log("arrowRight");
+};
+arrow_left.addEventListener("click", () => arrowLeft());
+arrow_right.addEventListener("click", () => arrowRight());
