3a8246200b7ac76f9dcad825df41996313464984
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-11-27 21:08:16 +0200

    style(script): remove defined unwanted tags

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,3 @@
-const header_paragraph_1 = document.getElementById("header_paragraph_1");
-const header_paragraph_2 = document.getElementById("header_paragraph_2");
-const header_paragraph_3 = document.getElementById("header_paragraph_3");
-const header_paragraph_4 = document.getElementById("header_paragraph_4");
 const arrow_left = document.getElementById("arrow_left");
 const arrow_right = document.getElementById("arrow_right");
 
